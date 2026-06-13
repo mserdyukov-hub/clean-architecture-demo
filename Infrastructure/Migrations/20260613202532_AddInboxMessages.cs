cf7515feb521cd1ae6af2c700984e4a650767b11
mserdyukov <mserdyukov@bostonsd.ru>
2026-06-13 23:26:15 +0300

    Add inbox migration

---
+++ Infrastructure/Migrations/20260613202532_AddInboxMessages.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddInboxMessages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "InboxMessage",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Topic = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    Partition = table.Column<int>(type: "integer", nullable: false),
+                    Offset = table.Column<long>(type: "bigint", nullable: false),
+                    ReceivedOnUtc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ProcessedOnUtc = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    Error = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InboxMessage", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InboxMessage_Topic_Partition_Offset",
+                table: "InboxMessage",
+                columns: new[] { "Topic", "Partition", "Offset" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "InboxMessage");
+        }
+    }
+}
